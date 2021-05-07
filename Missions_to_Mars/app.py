c904d7545a45d9b8cca3e4d70b027e299a916699
lucille bluth <melissa.mongrella@northwestern.edu>
2021-05-06 20:54:12 -0500

    skeleton of app.py file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_craigslist
+import scrape_mars
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 @app.route("/")
